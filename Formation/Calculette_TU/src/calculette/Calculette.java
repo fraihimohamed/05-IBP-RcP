cfac7d72cea029cbc7f110e1b49f5ce993b976ee
AnthonyCamusESAIP <anthony.camus@stjodijon.com>
2017-10-19 13:39:36 +0200

    Projet de test unitaires

---
+++ Formation/Calculette_TU/src/calculette/Calculette.java
@@ -0,0 +1,17 @@
+package calculette;
+
+public class Calculette {
+
+	public float addition(float a, float b) {
+		return a+b;
+	}
+	public float soustraction(float a, float b) {
+		return a-b;
+	}
+	public float diviser(float a, float b) {
+		return a/b;
+	}
+	public float multiplier(float a, float b) {
+		return a*b;
+	}
+}
