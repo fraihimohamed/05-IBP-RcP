ba38de10af41578830db35a661918c9a2663fed2
AnthonyCamusESAIP <anthony.camus@stjodijon.com>
2017-10-19 15:27:22 +0200

    Projet formation JSF

---
+++ Formation/Projet_Pratique/src/beans/MainClass.java
@@ -0,0 +1,16 @@
+package beans;
+
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+import org.primefaces.model.chart.PieChartModel;
+
+@SuppressWarnings("serial")
+@ManagedBean
+public class MainClass implements Serializable {
+
+	
+}
